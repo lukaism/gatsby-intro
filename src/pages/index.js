f4e1979051b7c0ef8e601d091906509e68575252
Luka Bontempi <luka.bontempi@gmail.com>
2020-08-23 12:44:20 +0200

    I built a blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,26 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import usePosts from '../hooks/use-posts'
+import PostPreview from '../components/post-preview'
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => {
+    const posts = usePosts();
+
+    return (
+        <>
+            <Hero />
+            <Layout >
+                <h1>Home</h1>
+                <p>Hello Geneva</p>
+                <Link to="/about/">Learn about me &rarr;</Link>
+
+                <h2>Read my blog</h2>
+                {posts.map(post => (
+                    <PostPreview key={post.slug} post={post} />
+                ))}
+            </Layout >
+        </>
+    );
+};
